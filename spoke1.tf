2b74f8607f714cd016a85e84f04341b293d7e8ee
alpayhassan1 <alpayhassan@hotmail.com>
2022-07-23 19:09:26 +0100

    setting up two spoke networks to act as two separate environments (development and testing). need to configure network peering between the hub and spokes

---
+++ spoke1.tf
@@ -0,0 +1,69 @@
+locals {
+  spoke1-rgname = "Development Environment"
+  spoke1-location = "uksouth"
+}
+
+resource "azurerm_resource_group" "spoke1-rg" {
+  name     = local.spoke1-rgname
+  location = local.spoke1-location
+}
+
+resource "azurerm_virtual_network" "spoke1-vnet" {
+  name                = "development-network"
+  address_space       = ["10.5.0.0/16"]
+  location            = local.spoke1-location
+  resource_group_name = local.spoke1-rgname
+}
+
+resource "azurerm_subnet" "spoke1-subnet" {
+  name                 = "development-default-subnet"
+  resource_group_name  = local.spoke1-rgname
+  virtual_network_name = azurerm_virtual_network.spoke1-vnet.name
+  address_prefixes     = ["10.5.0.0/24"]
+}
+
+
+# Creating development environment VMs
+resource "azurerm_network_interface" "spoke1-nic" {
+  name                = "spoke1-nic"
+  location            = local.spoke1-location
+  resource_group_name = local.spoke1-rgname
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "spoke1"
+    subnet_id                     = azurerm_subnet.spoke1-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "spoke1vm1" {
+  name                = "dev-vm-1"
+  location            = local.spoke1-location
+  resource_group_name = local.spoke1-rgname
+  size                = var.vmsize
+  admin_username      = var.username
+  network_interface_ids = [
+    azurerm_network_interface.spoke1-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = file("./linux-vmkey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+
+# VNet peering between this spoke and the hub
---
+++ spoke2.tf
@@ -0,0 +1,69 @@
+locals {
+  spoke2-rgname = "Testing Environment"
+  spoke2-location = "uksouth"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = local.spoke2-rgname
+  location = local.spoke2-location
+}
+
+resource "azurerm_virtual_network" "spoke2-vnet" {
+  name                = "testing-network"
+  address_space       = ["10.6.0.0/16"]
+  location            = local.spoke2-location
+  resource_group_name = local.spoke2-rgname
+}
+
+resource "azurerm_subnet" "spoke2-subnet" {
+  name                 = "testing-default-subnet"
+  resource_group_name  = local.spoke2-rgname
+  virtual_network_name = azurerm_virtual_network.spoke2-vnet.name
+  address_prefixes     = ["10.6.0.0/24"]
+}
+
+
+# Creating testing environment VMs
+resource "azurerm_network_interface" "spoke2-nic" {
+  name                = "spoke2-nic"
+  location            = local.spoke2-location
+  resource_group_name = local.spoke2-rgname
+  enable_ip_forwarding = true
+
+  ip_configuration {
+    name                          = "spoke2"
+    subnet_id                     = azurerm_subnet.spoke2-subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "spoke2vm1" {
+  name                = "testing-vm-1"
+  location            = local.spoke2-location
+  resource_group_name = local.spoke2-rgname
+  size                = var.vmsize
+  admin_username      = var.username
+  network_interface_ids = [
+    azurerm_network_interface.spoke2-nic.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.username
+    public_key = file("./linux-vmkey.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+
+# VNet peering between this spoke and the hub
